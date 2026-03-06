f75c9a24e519b58a4970f204c5fabdc56d183dd8
Ivosaja <ivoliobel@gmail.com>
2026-03-05 21:50:39 -0300

    add: conform plugin (formatters)

--- lua/ivox/lazy/autopairs.lua
+++ lua/ivox/lazy/autopairs.lua
@@ -1,20 +1,16 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = function ()
-	    local autopairs = require("nvim-autopairs")
-	    autopairs.setup({})
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		local autopairs = require("nvim-autopairs")
+		autopairs.setup({})
 
-	    -- integración con cmp
-	    local cmp = require("cmp")
-	    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		-- integración con cmp
+		local cmp = require("cmp")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-	    cmp.event:on(
-		    "confirm_done",
-		    cmp_autopairs.on_confirm_done()
-	    )
-    	
-    end
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
+	-- use opts = {} for passing setup options
+	-- this is equivalent to setup({}) function
 }
--- lua/ivox/lazy/lsp.lua
+++ lua/ivox/lazy/lsp.lua
@@ -3,62 +3,69 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp", -- completion
 		"hrsh7th/nvim-cmp", -- engine
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-cmdline',
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 		"mason-org/mason.nvim",
-		"mason-org/mason-lspconfig.nvim"
+		"mason-org/mason-lspconfig.nvim",
 	},
 	config = function()
-		local cmp = require('cmp')
-		local cmp_lsp = require('cmp_nvim_lsp')
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
 
 		local lspconfig = require("lspconfig")
-		local mason = require('mason').setup()
-		local mason_lspconfig = require('mason-lspconfig')
+		local mason = require("mason").setup()
+		local mason_lspconfig = require("mason-lspconfig")
 		local capabilities = cmp_lsp.default_capabilities()
 
 		mason_lspconfig.setup({
 			ensure_installed = {
 				"lua_ls",
 				"vimls",
-				"rust_analyzer"
+				"rust_analyzer",
+				"clangd",
+				"gopls",
+				"ts_ls",
 			},
 			handlers = {
-				function (server_name) -- whatever server you want
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities
-					})
+				function(server_name) -- whatever server you want
+					require("lspconfig")[server_name].setup({ capabilities = capabilities })
 				end,
-				["lua_ls"] = function ()
+				["lua_ls"] = function()
 					lspconfig.lua_ls.setup({
 						capabilities = capabilities,
 						settings = {
 							Lua = {
 								diagnostics = {
-									globals = {"vim"}
-								}
-
-							}
-						}
-
+									globals = { "vim" },
+								},
+							},
+						},
 					})
-				end
-			}
+				end,
+			},
 		})
 		-- configuring the cmd (completion)
 		cmp.setup({
 			mapping = cmp.mapping.preset.insert({
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(), -- quit the completion menu
-				['<Enter>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(), -- quit the completion menu
+				["<Enter>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
 			sources = {
 				{ name = "nvim_lsp" },
 				{ name = "buffer" },
-				{ name = "path" }
-			}
+				{ name = "path" },
+			},
 		})
-	end
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+			matching = { disallow_symbol_nonprefix_matching = false },
+		})
+	end,
 }
-
