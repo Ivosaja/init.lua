c483460db4e3c9f00c1fb0ed9463af5f5520f768
Ivosaja <ivoliobel@gmail.com>
2026-03-06 20:12:29 -0300

    add: new vim.option

--- lua/ivox/set.lua
+++ lua/ivox/set.lua
@@ -2,6 +2,7 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
--- This is used to yank from nvim to the system's clipboard -- 
+-- This is used to yank from nvim to the system's clipboard --
 vim.opt.clipboard = "unnamedplus"
-
+vim.opt.ignorecase = true
+vim.opt.cursorline = true
